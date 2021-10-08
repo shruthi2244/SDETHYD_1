c7211e714ad1194cb62ae1ed3bae648ef7d74b1e
Shruthi Badala <Shruthi Badala@LAPTOP-MNL8F5BN>
2021-10-08 15:33:49 +0530

    comm changes

--- SDETHYD_1/src/test/java/com/mmt/practise/propertyfile.java
+++ SDETHYD_1/src/test/java/com/mmt/practise/propertyfile.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 public class propertyfile {
 
 	public static void main(String[] args) throws Throwable {
-		FileInputStream fis=new FileInputStream("C:\\Users\\Shruthi Badala\\eclipse-workspace\\SDETHYD_1\\common.properties");
+		FileInputStream fis=new FileInputStream("./common.properties");
 		Properties pro=new Properties();
 		pro.load(fis);
 		String value=pro.getProperty("browser");
